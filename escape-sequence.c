c4d129b3d41bb5e628bfa3c7e09e6a33125585bc
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-07-10 15:52:43 +0530

    add

---
+++ escape-sequence.c
@@ -0,0 +1,13 @@
+/*
+\n - New line
+\e - escape
+\a - alarm (bell) sound
+\b - backSpace
+\f - format
+\v - vertical tab
+\t - horizontal tab
+\\ - to print \ 
+\" = to print "
+\' = to print '
+\? = to print ?
+*/
---
+++ variable.c
@@ -0,0 +1,7 @@
+//WAP to print static value 
+#include<stdio.h>
+void main()
+{
+    int no = 100;
+    printf("value of no is: %d", no);
+}
