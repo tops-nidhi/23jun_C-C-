ad3113787783bc78d3760bd6a603144edb779358
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-07-15 18:23:50 +0530

    add

---
+++ Operator/Arithmatic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    int ans;
+    //+, -, *, /, %, --, ++
+    printf("Enter the value of a and b: ");
+    scanf("%d%d", &a, &b);
+    --b;
+    printf("\nAddition is: %d", b);
+}
