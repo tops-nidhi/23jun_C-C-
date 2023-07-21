cfc4f512395777d69b95ae23266b7ca43df54337
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-07-21 15:43:18 +0530

    add

---
+++ Operator/Bitwise.c
@@ -0,0 +1,17 @@
+// Bitwise operator
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter the value of \nA = ");
+    scanf("%d", &a);
+    printf("\nB = ");
+    scanf("%d", &b);
+    // printf("\nBitwise AND is: %d", a&b);
+    // printf("\nBitwise OR is: %d", a|b);
+    // printf("\nBitwise XOR is: %d", a^b);
+    // printf("\nBitwise Compliment is: %d", ~a);
+    // printf("\nBitwise Shift left is: %d", a<<1);
+    printf("\nBitwise Shift right is: %d", b>>1);
+    return 0;
+}
