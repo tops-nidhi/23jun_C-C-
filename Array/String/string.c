f297d865e98f337b4c78d2b792b3f8c0fa02c77d
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-09-20 15:54:28 +0530

    add

--- Array/String/string.c
+++ Array/String/string.c
@@ -4,7 +4,7 @@
 int main()
 {
 
-    int length;
+    int length, cmp;
     char a[20], b[20];
     printf("Enter the character: ");
     scanf("%s",&a);
@@ -17,8 +17,18 @@ int main()
     // printf("\nYour copied string is: %s", b);
     printf("Enter the second string: ");
     scanf("%s",&b);
-    strcat(a,b);
-
-    printf("Your full string is: %s", a);
+    // strcat(a,b);
+    // printf("Your full string is: %s", a);
+    cmp = strcmp(a, b); //use to compare the two string
+    if (cmp == 0) {
+        printf("Both strings are equal");
+    }
+    else if(cmp > 0)
+    {
+        printf("String A is greater than B\n");
+    }
+    else{
+        printf("String B is greater than A \n");
+    }
     return 0;
 }
